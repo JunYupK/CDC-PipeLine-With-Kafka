6c2a9c90ee5aa0602a13074f47dcab8280ade76b
yongbul <top1506@naver.com>
2024-12-30 20:35:50 +0900

    window 오류로 인해 브랜치 추가

--- CrawlingServer/main.py
+++ CrawlingServer/main.py
@@ -13,19 +13,19 @@ async def hello_world():
 @app.get("/crwal")
 async def crwal_data():
     # 데이터 추출 스키마 정의
-    schema = {
-        "name": "Naver IT News",
-        "baseSelector": ".media_end_head_title",  # 각 뉴스 항목을 감싸는 컨테이너
-        "fields": [
-            {
-                "name": "title",
-                "selector": "h2.media_end_head_headline > span",
-                "type": "text"
-            }
-        ]
-    }
-
-    extraction_strategy = JsonCssExtractionStrategy(schema, verbose=True)
+    # schema = {
+    #     "name": "Naver IT News",
+    #     "baseSelector": ".section_component as_section_headline _PERSIST_CONTENT > div",  # 각 뉴스 항목을 감싸는 컨테이너
+    #     "fields": [
+    #         {
+    #             "name": "title",
+    #             "selector": "#_SECTION_HEADLINE_LIST_hqxz0",
+    #             "type": "text"
+    #         }
+    #     ]
+    # }
+    #
+    # extraction_strategy = JsonCssExtractionStrategy(schema, verbose=True)
 
     async with AsyncWebCrawler(
             headless=False,
@@ -33,7 +33,7 @@ async def crwal_data():
     ) as crawler:
 
         result = await crawler.arun(
-            url="https://n.news.naver.com/mnews/article/138/0002188585",
+            url="https://www.naver.com",
             extraction_strategy=JsonCssExtractionStrategy(schema, verbose=True),
             cache_mode=CacheMode.BYPASS
         )
@@ -47,6 +47,14 @@ async def crwal_data():
             print("Failed to parse JSON result")
             print("Raw result:", result.extracted_content)
 
+
+@app.get("/test")
+async def crwal_test():
+    async with AsyncWebCrawler(verbose=True) as crawler:
+        result = await crawler.arun(url="https://www.nbcnews.com/business")
+        # Soone will be change to result.markdown
+        print(result.markdown_v2.raw_markdown)
+        return  result
 
 @app.get("/health")
 async def health_check():
